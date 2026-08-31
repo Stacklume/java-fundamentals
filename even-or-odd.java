a62d41d086b613325b476b7d8318d077cf743a1c
Safa Syed <safa.syed2786@gmail.com>
2026-08-31 23:51:31 +0530

    Add even-or-odd program in Java

---
+++ even-or-odd.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    int n=17;
+    if(n%2==0){
+      System.out.print("Even Number");
+    }
+    else{
+      System.out.print("Odd Number");
+    }
+  }
+}   

f017ef698edb0da60ab2fdb58e56c90a3e114b03
Safa Syed <safa.syed2786@gmail.com>
2026-09-15 11:10:53 +0530

    Add linear search method in Solution class
    Implement linear search algorithm to find target index.

---
+++ Linear-Search.java
@@ -0,0 +1,12 @@
+/*Given an array of integers nums and an integer target, find the smallest index (0 based indexing)
+  where the target appears in the array. If the target is not found in the array, return -1*/
+class Solution {
+    public int linearSearch(int nums[], int target) {
+		for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                return i;
+            }       
+        }
+        return -1;
+    }
+}
